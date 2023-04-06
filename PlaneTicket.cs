ddb85737b50db9fd283841acb9124f6a5c1a1157
FaeruzzQ <128023607+FaeruzzQ@users.noreply.github.com>
2023-04-06 14:49:55 +0700

    Add files via upload

---
+++ PlaneTicket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class PlaneTicket
+    {
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public int cost { get; set; }
+        public string currency { get; set; }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin : {0}", origin);
+            Console.WriteLine("Destination : {0}", destination);
+            Console.WriteLine("Cost : {0} ({1})", cost, currency);
+            if (currency == "IDR")
+            {
+                cost = cost / 15000;
+                Console.WriteLine("Convert to Dollar = {0}", cost);
+                Console.WriteLine("\n");
+            } else if (currency == "USD")
+            {
+                cost = cost * 15000;
+                Console.WriteLine("Convert to Rupiah = {0}", cost);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket1.origin = "Jogja";
+            ticket1.destination = "PKU";
+            ticket1.cost = 100;
+            ticket1.currency = "USD";
+
+            ticket2.origin = "Jakarta";
+            ticket2.destination = "PKU";
+            ticket2.cost = 1500000;
+            ticket2.currency = "IDR";
+
+            ticket1.PrintSummary();
+            ticket2.PrintSummary();
+
+            Console.ReadKey();
+        }
+    }
+}
